7e0b782a7359e82fbc8b8155a2bde26734713360
Ankit Verma <ankitverma3103@gmail.com>
2019-08-25 19:08:02 +0530

    code format, comment and optimise

--- static/js/script.js
+++ static/js/script.js
@@ -1,56 +1,116 @@
-// $.getJSON("config.json", function(sitesConfig) {
-// chrome.storage.sync.set({quickLauncherSitesConfig:sitesConfig});
-// initializeCards(sitesConfig);
-// });
+/** Get stored sites config from chrome sync storage */
+getSitesConfigFromChromeSyncStorage("quickLauncherSitesConfig");
 
-chrome.storage.sync.get(["quickLauncherSitesConfig"], function(config) {
-  // initializeCards(config.quickLauncherSitesConfig);
-  if (
-    config["quickLauncherSitesConfig"] &&
-    Object.keys(config["quickLauncherSitesConfig"]).length > 0
-  ) {
-    initializeCards(config.quickLauncherSitesConfig);
-    $("#jsonSiteDataTextArea").val(
-      JSON.stringify(config.quickLauncherSitesConfig, null, 2)
-    );
-  }
-});
+/** initially hide the second screen(add JSON) */
+hideTextAreaScreen();
 
-$(".add-site-container").hide();
-$(".save-site-icon").hide();
+/**
+ * @description get stored sites JSON config from chrome sync storage
+ * and initialize the cards
+ * @param {string} key - name of key to retrive config
+ */
+function getSitesConfigFromChromeSyncStorage(key) {
+  chrome.storage.sync.get([key], siteConfig => {
+    /** if config found, initialize the cards */
+    if (siteConfig[key]) {
+      initializeCards(siteConfig[key]);
 
-$("#addSiteIcon").click(() => {
-$("#jsonSiteDataTextArea").attr('rows', 30);
-  toggleElements();
-});
-
-$("#saveSiteIcon").click(() => {
-  chrome.storage.sync.set(
-    { quickLauncherSitesConfig: JSON.parse($("#jsonSiteDataTextArea").val()) },
-    function() {
-      chrome.storage.sync.get(["quickLauncherSitesConfig"], function(config) {
-        if (
-          config["quickLauncherSitesConfig"] &&
-          Object.keys(config["quickLauncherSitesConfig"]).length > 0
-        ) {
-          $(".cards-container").empty();
-          initializeCards(config["quickLauncherSitesConfig"]);
-        }
-      });
+      /** Populate the text area with config */
+      populateTextArea(siteConfig[key]);
     }
-  );
-$("#jsonSiteDataTextArea").attr('rows', 5);
-  toggleElements();
-});
+  });
+}
 
-function toggleElements() {
+/**
+ * @description save the sites config in chrome sync storage
+ * @param {string} key - name of key
+ * @param {Object} config - sites config
+ */
+function setSitesConfigToChromeSyncStorage(key, config){
+  chrome.storage.sync.set({[key] : config});
+}
+
+/**
+ * @description - populates the text-area with JSON object
+ * @param {Object} config - sites config
+ */
+function populateTextArea(config) {
+  $("#jsonSiteDataTextArea").val(
+    JSON.stringify(config, null, 2)
+  );
+}
+
+/** Hide the add JSON screen HTML elements */
+function hideTextAreaScreen() {
+  $("#addSiteDataContainer").hide();
+  $("#saveSiteIcon").hide();
+  $("#jsonSiteDataError").hide();
+  $("#goBack").hide();
+}
+
+/** Hide/Show specific elements based on current screen */
+function toggleScreenElements() {
   $(".cards-container").toggle();
   $(".add-site-container").toggle();
   $(".save-site-icon").toggle();
   $(".add-site-icon").toggle();
+  $("#goBack").toggle();
 }
 
+/** Show add site data screen on '+' click */
+$("#addSiteIcon").click(() => {
+  $("#jsonSiteDataTextArea").attr("rows", 25);
+  toggleScreenElements();
+});
+
+/** Show cards screen(home screen) on '<-'(go back) click */
+$("#goBack").click(() => {
+  toggleScreenElements();
+});
+
+/** On save click */
+$("#saveSiteIcon").click(() => {
+  /** if valid JSON Object entered in textarea */
+  if ($("#jsonSiteDataTextArea").val() && ValidateJSON($("#jsonSiteDataTextArea").val())) {
+    /** save config to chrome sync storage */
+    setSitesConfigToChromeSyncStorage("quickLauncherSitesConfig", 
+    JSON.parse($("#jsonSiteDataTextArea").val()));
+    /** reset the cards */
+    $(".cards-container").empty();
+
+    /** initialize the cards with new data */
+    getSitesConfigFromChromeSyncStorage("quickLauncherSitesConfig");
+
+    /** decrease the rows in textarea to decrease height */
+    $("#jsonSiteDataTextArea").attr("rows", 5);
+
+    /** show the homescreen */
+    toggleScreenElements();
+  }
+});
+
+/** validates JSON and shows errors */
+function ValidateJSON(json) {
+  var checkedjson;
+  try {
+    checkedjson = JSON.parse(json);
+  } catch (e) {
+    /** set error */
+    $("#jsonSiteDataError").show().text(e.message);
+
+    /** hide error after 10 secs */
+    setTimeout(() => {
+      $("#jsonSiteDataError").hide();
+    }, 8000);
+  }
+  return checkedjson;
+}
+
+/** initializes the cards */
 function initializeCards(sitesConfig) {
+  /** hide no data text */
+  $("#noSiteData").hide();
+  /** loop through config */
   Object.keys(sitesConfig).forEach(site => {
     let siteConfig = sitesConfig[site];
     $(".cards-container").append(
@@ -61,7 +121,6 @@ function initializeCards(sitesConfig) {
         '"></div></div></div>'
     );
     Object.keys(siteConfig).forEach(environment => {
-      // console.log(environment + ':' + siteConfig[environment]);
       $("." + site).append(
         "<a href=" +
           siteConfig[environment] +
